273fc58f36101ba46cd3a37b44f2f142d844f651
burkayakgul <burkayakgul@hotmail.com>
2021-11-20 21:34:16 +0300

    Kaynak dosyalarına npm ile bootstrap ve fontawesome eklendi. Websitesi arayüzü için template eklendi ve template'de gerekli linklerde düzeltmeler yapıldı.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    //
+    public function Index()
+    {
+        return view("home/index");
+    }
+
+    public function test($id)
+    {
+        echo "İd: " , $id;
+    }
 }
